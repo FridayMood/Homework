290e3df9c4646c9e75afe0198935fede81a09c65
amelnikava <iamaliaksandra@gmail.com>
2023-07-05 15:31:36 +0200

    changes in add2

--- src/lesson2/Additional2.java
+++ src/lesson2/Additional2.java
@@ -3,6 +3,7 @@ package lesson2;
 public class Additional2 {
     public static void main(String[] args) {
         System.out.println ("New Task");
+        System.out.println ("New Task");
     }
 
 }
